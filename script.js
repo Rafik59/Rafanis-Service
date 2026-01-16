8df6383335a96af9e195f5b24420923adb744d11
Rafik59 <srafikdu59@gmail.com>
2026-01-16 14:45:19 +0100

    script.js

--- script.js
+++ script.js
@@ -12,3 +12,54 @@ window.addEventListener("click", function (e) {
     closeContact();
   }
 });
+
+/* ================= OUVERT / FERME ================= */
+function checkOpenStatus() {
+  const now = new Date();
+  const day = now.getDay(); // 0 = dimanche
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  const time = hour + minute / 60;
+
+  let open = false;
+
+  // Lundi
+  if (day === 1) {
+    open = time >= 13.5 && time <= 19.5;
+  }
+
+  // Mardi à Jeudi
+  if (day >= 2 && day <= 4) {
+    open =
+      (time >= 9.5 && time <= 12) ||
+      (time >= 13.5 && time <= 19.5);
+  }
+
+  // Vendredi
+  if (day === 5) {
+    open =
+      (time >= 9.5 && time <= 12) ||
+      (time >= 14 && time <= 19.5);
+  }
+
+  // Samedi
+  if (day === 6) {
+    open =
+      (time >= 9.5 && time <= 12) ||
+      (time >= 13.5 && time <= 19.5);
+  }
+
+  const status = document.getElementById("status");
+
+  if (open) {
+    status.className = "status open";
+    status.innerHTML = "🟢 Ouvert";
+  } else {
+    status.className = "status closed";
+    status.innerHTML = "🔴 Fermé";
+  }
+}
+
+checkOpenStatus();
+setInterval(checkOpenStatus, 60000);
+
